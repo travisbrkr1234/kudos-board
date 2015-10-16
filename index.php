8b5daf7814ae8cb8908bba70d4786b4d4ea01729
travisbrkr1234 <carlos.ochoa@infusionsoft.com>
2015-10-16 15:27:04 -0700

    added message randomization, tested live

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 
   }
 
-  $query = "SELECT * FROM kudos_message LIMIT 4";
+  $query = "SELECT * FROM messages ORDER BY RAND() LIMIT 4";
 
   $newConnections = mysql_query($query);
 
@@ -42,19 +42,29 @@
 
   echo "<table>\n";
 
-  while ($row = mysql_fetch_assoc($newConnections)) {
-    echo "<tr><td>";
-    echo $row['Message'] . "\n";
-    echo "</td></tr>";
-    echo "<tr><td>";
-    echo " -" . $row['Submitter'] . "\n";
-    echo "</td></tr>";
-  }
-  echo "</table>";
+    while ($row = mysql_fetch_assoc($newConnections)) {
+      echo "<tr><td>";
+        echo $row['message'] . "\n";
+        echo "</td></tr>";
+        echo "<tr><td>";
+          echo " -" . $row['submitter'] . "\n";
+          echo "</td></tr>";
+        }
+        echo "</table>";
 
-  mysql_close();
+        mysql_close();
 
-  ?>
+        ?>
+        <script charset="utf-8">
+        //colorChange start
+        var bg = "#";
+        var options = ['125079',  '51A3DC',  '79BE37',  'A05D8E',  '646771']
 
-</body>
-</html>
+        for (var i = 0 ; i < 1 ; i++) {
+          bg += options[Math.floor(Math.random()*5)];
+        }
+        document.getElementsByTagName('body')[0].style.background = bg;
+
+        </script>
+      </body>
+      </html>
